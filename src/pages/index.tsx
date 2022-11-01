3553b4fd134b2fedbe4881eb0828a466616ac436
Caio Vinícius <caio1525pereira@gmail.com>
2022-11-01 18:06:09 -0300

    feat: :iphone: Responsividade da landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,24 +7,24 @@ import iconCheckImg from "../assets/icon-check.svg";
 
 export default function Home() {
 	return (
-		<div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 items-center gap-28">
+		<div className="max-w-[450px] px-4 py-10 h-full mx-auto grid grid-cols items-center gap-28 md:px-0 xl:grid-cols-2 md:max-w-[600px] xl:max-w-[1124px] xs:h-screen">
 			<main>
 				<Image src={logoImg} alt="NLW Copa" />
 
-				<h1 className="mt-14 text-white text-5xl font-bold leading-tight">
+				<h1 className="mt-14 text-white text-3xl font-bold leading-tight md:text-5xl">
 					Crie seu próprio bolão da copa e compartilhe entre amigos!
 				</h1>
 
 				<div className="mt-10 flex items-center gap-2">
 					<Image src={usersAvatarExampleImg} alt="" />
 
-					<strong className="text-gray-100 text-xl">
+					<strong className="text-gray-100 text-base md:text-xl">
 						<span className="text-ignite-500">+12.592</span> pessoas já estão
 						usando
 					</strong>
 				</div>
 
-				<form className="mt-10 flex gap-2">
+				<form className="mt-10 flex flex-col gap-2 sm:flex-row">
 					<input
 						className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm"
 						type="text"
@@ -45,21 +45,21 @@ export default function Home() {
 					para convidar outras pessoas 🚀
 				</p>
 
-				<div className="mt-10 pt-10 border-t border-gray-600 flex justify-between text-gray-100">
-					<div className="flex items-center gap-6">
-						<Image src={iconCheckImg} alt="" />
+				<div className="mt-10 pt-10 border-t border-gray-600 flex justify-evenly text-gray-100 text-center xs:text-start xs:justify-between">
+					<div className="flex flex-col items-center gap-3 xs:gap-6 xs:flex-row">
+						<Image src={iconCheckImg} alt="" className="w-8 xs:w-10" />
 						<div className="flex flex-col">
-							<span className="font-bold text-2xl">+2.034</span>
+							<span className="font-bold text-sm xs:text-2xl">+2.034</span>
 							<span>Bolões criados</span>
 						</div>
 					</div>
 
-					<div className="w-px h-14 bg-gray-600"></div>
+					<div className="w-px h-24 bg-gray-600 xs:h-14"></div>
 
-					<div className="flex items-center gap-6">
-						<Image src={iconCheckImg} alt="" />
+					<div className="flex flex-col items-center gap-3 xs:gap-6 xs:flex-row">
+						<Image src={iconCheckImg} alt="" className="w-8 xs:w-10" />
 						<div className="flex flex-col">
-							<span className="font-bold text-2xl">+192.847</span>
+							<span className="font-bold text-sm xs:text-2xl">+192.847</span>
 							<span>Palpites enviados</span>
 						</div>
 					</div>
@@ -70,6 +70,7 @@ export default function Home() {
 				src={appPreviewImg}
 				alt="Dois celulares exibindo uma prévia da aplicação móvel do NLW Copa"
 				quality={100}
+				className="hidden xl:block"
 			/>
 		</div>
 	);
